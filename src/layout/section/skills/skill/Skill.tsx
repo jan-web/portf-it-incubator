a1079dc9ec9b8b760077619bb92df4ab9645cab2
Yan <lookinmysocial@networks.com>
2024-09-11 13:16:04 +0200

    feat: The block Testimomy has been styled

--- src/layout/section/skills/skill/Skill.tsx
+++ src/layout/section/skills/skill/Skill.tsx
@@ -36,7 +36,7 @@ const SkillTitle = styled.h3`
 const SkillText = styled.p`
   text-align: center;
 `
-const IconWrapper = styled.div`
+export const IconWrapper = styled.div`
   position: relative;
 
   &::before {
--- src/layout/section/testimony/Testimony.tsx
+++ src/layout/section/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import { SectionTitle } from '../../../components/SectionTitle'
 import { Icon } from '../../../components/Icon/icon'
 import { Slider } from '../../../components/slider/Slider'
 import { FlexWrapper } from '../../../components/FlexWrapper'
+import { IconWrapper } from '../skills/skill/Skill'
 
 export const Testimony = () => {
   return (
     <StyledTestimony>
       <SectionTitle>Testimony</SectionTitle>
       <FlexWrapper direction={'column'} align={'center'}>
-        <Icon iconId={'commas'} />
+        <IconWrapper>
+          <Icon iconId={'commas'} />
+        </IconWrapper>
         <Slider />
       </FlexWrapper>
 
@@ -20,4 +23,10 @@ export const Testimony = () => {
 
 const StyledTestimony = styled.section`
   background-color: #aeb9ff;
+  min-height: 50vh;
+
+
+  ${IconWrapper} {
+    margin: 40px 0 72px;
+  }
 `
