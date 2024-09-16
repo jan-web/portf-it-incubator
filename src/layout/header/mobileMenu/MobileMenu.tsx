8c23f1f087ba249e67f5f91f84e3ae58b1c021f0
Yan <capsoloemail@gmail.com>
2024-09-16 18:09:07 +0200

    feat: The Header burger added

--- src/layout/header/mobileMenu/MobileMenu.tsx
+++ src/layout/header/mobileMenu/MobileMenu.tsx
@@ -1,47 +1,122 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme } from '../../../styles/Theme';
-export const MobileMenu = (props: {menuItems: Array<string>}) => {
-	return (
-		<StyledMobileMenu>
-      <BurgerButton>
-        
+export const MobileMenu = (props: { menuItems: Array<string> }) => {
+  return (
+    <StyledMobileMenu>
+      <BurgerButton isOpen={true}>
+        <span></span>
       </BurgerButton>
-			<ul>
-				{props.menuItems.map((item, index) =>
-				(<ListItem key={index}>
-					<Link href={item}>
-          {item}
-          <Mask>
-            <span>{item}</span>
-          </Mask>
-          <Mask>
-            <span>{item}</span>
-          </Mask>
-          </Link>
-				</ListItem>)
-				)}
+      <MobileMenuPopup isOpen={true}>
+        <ul>
+          {props.menuItems.map((item, index) =>
+          (<ListItem key={index}>
+            <Link href={item}>
+              {item}
+              <Mask>
+                <span>{item}</span>
+              </Mask>
+              <Mask>
+                <span>{item}</span>
+              </Mask>
+            </Link>
+          </ListItem>)
+          )}
 
 
-			</ul>
-		</StyledMobileMenu>
-	);
+        </ul>
+      </MobileMenuPopup>
+
+    </StyledMobileMenu>
+  );
 };
 
 const StyledMobileMenu = styled.nav`
+  display: none;
+
+  @media ${theme.media.tablet} {
+    display: block;
+  }
+`
+
+const MobileMenuPopup= styled.div<{isOpen: boolean}>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99999;
+  background-color: rgba(31, 31, 32, 0.9);
+  display: none;
+
+  ${props => props.isOpen && css<{isOpen: boolean}> `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `}
+
 	ul {
 		display: flex;
 		gap: 30px;
 		justify-content: center;
+    align-items: center;
+    flex-direction: column;
 	}
 
-  @media ${theme.media.tablet} {
-    display: none;
-  }
+
+
 `;
 
-const BurgerButton = styled.button `
+const BurgerButton = styled.button<{isOpen: boolean}>`
+  position: fixed;
+  top: -100px;
+  right: -100px;
+  width: 200px;
+  height: 200px;
+  z-index: 9999999;
 
+  span {
+    display: block;
+    width: 36px;
+    height: 2px;
+    background-color: ${theme.colors.font};
+    position: absolute;
+    left: 40px;
+    bottom: 50px;
+
+    ${props => props.isOpen && css<{isOpen: boolean}> `
+      background-color: rgba(255,255, 255, 0);
+    `}
+
+    &::before {
+      content: '';
+      display: block;
+      width: 36px;
+      height: 2px;
+      background-color: ${theme.colors.font};
+      position: absolute;
+      transform: translateY(-10px);
+
+      ${props => props.isOpen && css<{isOpen: boolean}> `
+        transform: rotate(-45deg) translateY(0);
+      `}
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      width: 24px;
+      height: 2px;
+      background-color: ${theme.colors.font};
+      position: absolute;
+      transform: translateY(10px);
+
+      ${props => props.isOpen && css<{isOpen: boolean}> `
+        transform: rotate(45deg) translateY(0);
+        width: 36px;
+      `}
+    }
+  }
 `
 
 const Link = styled.a`
